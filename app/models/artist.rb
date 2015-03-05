13408402207d4d6cb36f07b7e8815574606514c9
cocoswank <patchkatie@gmail.com>
2015-03-05 12:16:27 -0600

    Added validations to models

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,6 @@
 class Artist < ActiveRecord::Base
   belongs_to :genre
   has_many :songs
+
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  validates :name, presence: true, uniqueness: true
+
 end
